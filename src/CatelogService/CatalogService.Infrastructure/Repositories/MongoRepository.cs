697c6a10f506f688a8b2579149c06257b402e2b3
GameTweak <esbendam@live.dk>
2025-09-21 21:25:48 +0200

    A few comments for elaboration

--- src/CatelogService/CatalogService.Infrastructure/Repositories/MongoRepository.cs
+++ src/CatelogService/CatalogService.Infrastructure/Repositories/MongoRepository.cs
@@ -6,6 +6,12 @@ using MongoDB.Driver;
 
 namespace CatalogService.Infrastructure.Repositories;
 
+/// <summary>
+/// A generic repository which is implemented to use MongoDb. It provides us with
+/// the classic CRUD operations. Though I've hardcoded, so the only real type it
+/// accepts is the CatalogItem
+/// </summary>
+/// <typeparam name="T"></typeparam>
 public class MongoRepository<T> : MongoDbContext, IRepository<T> where T : CatalogItem
 {
     private readonly IMongoCollection<T> _collection;
@@ -18,11 +24,13 @@ public class MongoRepository<T> : MongoDbContext, IRepository<T> where T : Catal
     /// <summary>
     /// Retrieves all entities of the provided type from the database
     /// </summary>
-    /// <returns></returns>
+    /// <returns>A collection of entities</returns>
     public async Task<Result<IEnumerable<T>>> GetAllAsync()
     {
         try
         {
+            // Since we already have defined the connection and collection
+            // we can simply retrievve the entities with Find()
             var items = await _collection.Find(_ => true).ToListAsync();
 
             return Result.Ok(items.AsEnumerable());
@@ -34,14 +42,16 @@ public class MongoRepository<T> : MongoDbContext, IRepository<T> where T : Catal
     }
 
     /// <summary>
-    /// Retrieves the first entity of the provided type with matching id
+    /// Retrieves the first entity with the provided id.
     /// </summary>
     /// <param name="id">The id of the type</param>
-    /// <returns></returns>
+    /// <returns>The entity if found</returns>
     public async Task<Result<T>> GetByIdAsync(int id)
     {
         try
         {
+            // Again, since the collection is defined we just need to 
+            // specify which entity to retrieve
             var query = await _collection.FindAsync(e => e.Id == id);
 
             return Result.Ok(query.FirstOrDefault());
@@ -56,7 +66,7 @@ public class MongoRepository<T> : MongoDbContext, IRepository<T> where T : Catal
     /// Creates a new entity of the provided type and saves it
     /// </summary>
     /// <param name="entity">The entity that is to be created</param>
-    /// <returns></returns>
+    /// <returns>A result indicating success or failure</returns>
     public async Task<Result> CreateAsync(T entity)
     {
         try
@@ -75,16 +85,16 @@ public class MongoRepository<T> : MongoDbContext, IRepository<T> where T : Catal
     /// Updates an already existing entity with provided one
     /// </summary>
     /// <param name="entity">The entity which should be updated</param>
-    /// <returns></returns>
+    /// <returns>A result indicating success or failure</returns>
     public async Task<Result> UpdateAsync(T entity)
     {
         try
         {
+            // This one stands out, since ReplaceOneAsync() requires
+            // us to define a filter, for it to get the entity.
             var filter = Builders<T>.Filter.Eq(e => e.Id, entity.Id);
-            var options = new ReplaceOptions { IsUpsert = true };
             
-            
-            await _collection.ReplaceOneAsync(filter, entity, options);
+            await _collection.ReplaceOneAsync(filter, entity);
             
             return Result.Ok();
         } catch (Exception)
@@ -97,7 +107,7 @@ public class MongoRepository<T> : MongoDbContext, IRepository<T> where T : Catal
     /// Removes the entity matching the id provided 
     /// </summary>
     /// <param name="id">The id of the entity to be deleted</param>
-    /// <returns></returns>
+    /// <returns>A result indicating success or failure</returns>
     public async Task<Result> DeleteAsync(int id)
     {
         try
